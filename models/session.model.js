ecf5883eb2aafc5409ec0135f8f3e89743a99463
pushpendu <pushpendu.ghosh@apptware.com>
2024-12-31 13:14:09 +0530

    added title description and tags at the seesion schema

--- models/session.model.js
+++ models/session.model.js
@@ -4,6 +4,9 @@ import mongoose from "mongoose";
 const sessionSchema = new mongoose.Schema({
     language: { type: String, required: true },
     content: { type: String, required: true },
+    title: { type: String, required: false },
+    description: { type: String, required: false },
+    tags: { type: [String], required: false },
     userId: { type: mongoose.Types.ObjectId, required: false, ref: 'User' }
 }, {
     timestamps: true
